2082b58504fe98554ea6939b9a86f4479147238f
pconrad <pconrad@cs.ucsb.edu>
2021-03-31 18:38:51 +0000

    Deploying to main from @ ucsb-cs156-s21/demo-react-minimal@36fcf551778220119b8a519e09d29d4b619ebdc1 🚀

---
+++ docs/storybook/precache-manifest.ba9738b3795f189f76b54c1eecdb9231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dacc8a7785fe175682d82f0a41ca038",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.034ee41c17b3f515c7db.bundle.js"
+  },
+  {
+    "url": "runtime~main.034ee41c17b3f515c7db.bundle.js"
+  },
+  {
+    "revision": "9edead8372cc4f90162e",
+    "url": "static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f80804e1afcf65bf353b",
+    "url": "static/css/vendors~main.32420154.chunk.css"
+  },
+  {
+    "url": "vendors~main.034ee41c17b3f515c7db.bundle.js"
+  },
+  {
+    "url": "vendors~main.034ee41c17b3f515c7db.bundle.js.LICENSE.txt"
+  }
+]);
